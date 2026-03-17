8c7d587590417345cda66d3474dfcd1e4e5a935f
Odyce <ulysse.decottignies@epitech.eu>
2026-03-17 23:55:28 +0100

    First weapon bug fixed

--- hellsing_undead_or_applive/lib/domain/inventory/weapons_model.dart
+++ hellsing_undead_or_applive/lib/domain/inventory/weapons_model.dart
@@ -319,4 +319,11 @@ class Weapon {
       startingWeapon: json["startingWeapon"],
     );
   }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) || other is Weapon && runtimeType == other.runtimeType && id == other.id;
+
+  @override
+  int get hashCode => id.hashCode;
 }
